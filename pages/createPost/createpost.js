c2dcbf1b2cc03bf65c88bc4ddef493060db7c026
sp-SwatiPatel <sp.swati2002@gmail.com>
2024-02-17 14:13:20 +0530

    updated files

--- pages/createPost/createpost.js
+++ pages/createPost/createpost.js
@@ -5,33 +5,7 @@ let currentUser = "";
 let url = "";
 let fileType = "";
 let done = document.getElementById("done");
-let uid;
-
-// let uploadimg = (event) => {
-//   fileType = event.target.files[0].fileType;
-//   var uploadTask = firebase.storage().ref().child(`posts/${event.target.files[0].name}`);
-//   uploadTask.on(
-//     "state_changed",
-//     (snapshot) => {
-//       var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-//       var uploadpercentage = Math.round(progress);
-//       progressDiv.style.display = "block";
-//       progressbar.style.width = `${uploadpercentage}%`;
-//       progressbar.innerHTML = `${uploadpercentage}%`;
-//     },
-//     (error) => {
-//       // Handle unsuccessful uploads
-//     },
-//     () => {
-
-//       uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
-//         url = downloadURL;
-//         done.style.display = "block";
-//         progressDiv.style.display = "none";
-//       });
-//     }
-//   );
-// }
+let uid = "";
 
 let uploadimg = (event) => {
   fileType = event.target.files[0].type;
@@ -50,7 +24,7 @@ let uploadimg = (event) => {
       progressBar.style.width = `${uploadpercentage}%`;
       progressBar.innerHTML = `${uploadpercentage}%`;
     },
-    (error) => { },
+    (error) => {},
     () => {
       uploadfile.snapshot.ref.getDownloadURL().then((downloadURL) => {
         url = downloadURL;
@@ -60,3 +34,41 @@ let uploadimg = (event) => {
     }
   );
 };
+
+
+var d = new Date().toLocaleDateString();
+
+
+function createpost() {
+  if (postValue.value !== " " || url !== " ") {
+    firebase
+      .firestore()
+      .collection("posts")
+      .add({
+        postValue: postValue.value,
+        uid: currentUser.uid,
+        url: url,
+        filetype: fileType,
+        like: "",
+        dislikes: "",
+        comments: "",
+        Date: `${d}`
+      })
+      .then((res) => {
+        firebase
+          .firestore()
+          .collection("posts/")
+          .doc(res.id)
+          .update({
+            id: res.id
+          })
+          .then(() => {
+            done.style.display = "none"
+            document.getElementById("uploadedmssage").style.display = "block";
+            setTimeout(() => {
+              location.reload();
+            }, 1000);
+          });
+      });
+  }
+}
