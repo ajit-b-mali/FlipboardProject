b78d3acd49d1b0a4c8568f18efececf2308eee66
sp-SwatiPatel <158474426+sp-SwatiPatel@users.noreply.github.com>
2024-02-22 12:14:47 +0000

    added: createPost

---
+++ pages/createPostNew/firebase.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC4Od-APlSrnyAO9vz7wPxYcDDMKcZAJZA",
+  authDomain: "learnfirebase-660de.firebaseapp.com",
+  projectId: "learnfirebase-660de",
+  storageBucket: "learnfirebase-660de.appspot.com",
+  messagingSenderId: "296838660557",
+  appId: "1:296838660557:web:14de50e0a6b581d97bc484",
+  measurementId: "G-7JJ2762X0P",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
