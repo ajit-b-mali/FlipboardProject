aa392116e5eb6ab6365d0ca4cae3c638f183ac10
sp-SwatiPatel <sp.swati2002@gmail.com>
2024-04-05 07:40:26 +0530

    people page updated

---
+++ pages/people/people.js
@@ -0,0 +1,26 @@
+const cardDiv = document.querySelector("#profile-cards");
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    firebase
+      .firestore()
+      .collection("users")
+      .onSnapshot((users) => {
+        cardDiv.innerHTML = "";
+        users.forEach((user) => {
+          let card = document.createElement("div");
+          card.classList.add("card");
+          card.innerHTML = `
+          <a href="userdetail.html?uid=${user.data()["uid"]}">
+          <h2 class="description">${
+            user.data()["FirstName"]}</h2>
+            <div class="image">
+              <img src="${user.data()["profilePicture"]}" alt=" " />
+            </div>
+          </a>
+          `;
+          cardDiv.appendChild(card);
+        });
+      });
+  }
+});
